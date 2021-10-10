d00001f4658328f55e52a555669126f905bd478a
Your Name <advmenendez92@gmail.com>
2021-10-10 17:14:18 +0200

    second commit

--- Iteration1.js
+++ Iteration1.js
@@ -1,27 +1,23 @@
-/* 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
-evento click que ejecute un console log con la información del evento del click */
 function addListeners() {
-    document.querySelector("#btnToClick").addEventListener("click", sayHello);
-    document.querySelector(".focus").addEventListener("click", inputOne);
-    document.querySelector(".value").addEventListener("click", inputTwo);
+    document.querySelector("#btnToClick").addEventListener("click", clickame);
+    document.querySelector(".focus").addEventListener("Escribe", inputOne);
+    document.querySelector(".value").addEventListener("Escribe", inputTwo);
 }
 
 window.onload = function () {
     addListeners();
 }
 
-function sayHello() {
+function clickame() {
     console.log("Acabas de hacer click en el botón.");
 }
 
 function inputOne() {
-    let formLogin = document.getElementsByClassName('focus');
-    console.log(input);
+    let formFocus = document.getElementsByClassName('focus');
+    if(formFocus=="Hola")
+    console.log("Hola");
 }
 function inputTwo() {
-    let formLogin = document.getElementsByClassName('value');
+    let formValue= document.getElementsByClassName('value');
     console.log(input);
 }
-//1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
-
-//1.3 Añade un evento 'input' que ejecute un console.log con el valor del input
